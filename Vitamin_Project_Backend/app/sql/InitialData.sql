44d4479a900ac09bd5030745739c8ab0ed408414
--add <sumit.pradhan@digimantra.com>
2023-01-20 18:22:51 +0530

    Completed Tasks

--- Vitamin_Project_Backend/app/sql/InitialData.sql
+++ Vitamin_Project_Backend/app/sql/InitialData.sql
@@ -3,7 +3,7 @@ INSERT INTO tabs (tab_id,name, display_name) VALUES (1,'Vitamin_D','Vitamin D');
 INSERT INTO tabs (tab_id,name, display_name) VALUES (2,'Supplements','Supplements');
 INSERT INTO tabs (tab_id,name, display_name) VALUES (3,'Nutrients','Nutrients');
 INSERT INTO tabs (tab_id,name, display_name) VALUES (4,'Spices','Spices');
-INSERT INTO tabs (tab_id,name, display_name) VALUES (5,'Common_Food_Allergy','Common Food Allergy');
+INSERT INTO tabs (tab_id,name, display_name) VALUES (5,'Common_Food_Allergies','Common Food Allergies');
 INSERT INTO tabs (tab_id,name, display_name) VALUES (6,'Restaurants','Restaurants');
 INSERT INTO tabs (tab_id,name, display_name) VALUES (7,'Recipes','Recipes');
 INSERT INTO tabs (tab_id,name, display_name) VALUES (8,'Pollution','Pollution');
@@ -17,7 +17,7 @@ INSERT INTO tab_childs (tab_child_id,name, display_name,tab_id) VALUES (2,'Sunsh
 INSERT INTO tab_childs (tab_child_id,name, display_name,tab_id) VALUES (3,'Supplements','Supplements',2);
 INSERT INTO tab_childs (tab_child_id,name, display_name,tab_id) VALUES (4,'Nutrients','Nutrients',3);
 INSERT INTO tab_childs (tab_child_id,name, display_name,tab_id) VALUES (5,'Spices','Spices',4);
-INSERT INTO tab_childs (tab_child_id,name, display_name,tab_id) VALUES (6,'Common_Food_Allergy','Common Food Allergy',5);
+INSERT INTO tab_childs (tab_child_id,name, display_name,tab_id) VALUES (6,'Common_Food_Allergies','Common Food Allergies',5);
 INSERT INTO tab_childs (tab_child_id,name, display_name,tab_id) VALUES (7,'Restaurants','Restaurants',6);
 INSERT INTO tab_childs (tab_child_id,name, display_name,tab_id) VALUES (8,'Recipes','Recipes',7);
 INSERT INTO tab_childs (tab_child_id,name, display_name,tab_id) VALUES (9,'Pollution','Pollution',8);
