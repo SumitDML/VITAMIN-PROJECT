531d03784b654055f87e9ff6aa914de66aa5bc44
--add <sumit.pradhan@digimantra.com>
2023-01-30 16:15:04 +0530

    Completed Tasks Final

--- VITAMIN_DEMO/Project_Backend/settings.py
+++ VITAMIN_DEMO/Project_Backend/settings.py
@@ -124,9 +124,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    # BASE_DIR / "static",
-    # '/var/www/static/',
+    os.path.join(BASE_DIR, 'static'),
+    '/home/dml-sumit/Desktop/Vitamin Project Final/VITAMIN-PROJECT/VITAMIN_DEMO/app/static',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
